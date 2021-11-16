ce49b5af57aa679bbc20ffc6d3b766974290a368
BootEXE <raphael_attal@icloud.com>
2021-11-16 21:47:09 +0100

    adding jquery support

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
+const $ = require( "jquery" );
+
 console.log('hello world');
+
+
+$('h1').on('click',function(e){
+    console.log('click');
+    if($(e.currentTarget).hasClass('active')){
+        $(e.currentTarget).removeClass('active');
+    }else{
+        $(e.currentTarget).addClass('active');
+    }
+});
