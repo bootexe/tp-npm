08feb75320f6ce303a0c6f7195f32558c0c0d2c5
BootEXE <raphael_attal@icloud.com>
2021-11-21 22:56:36 +0100

    mise a jours du cours

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ const $ = require( "jquery" );
 
 console.log('hello world');
 
-
 $('h1').on('click',function(e){
     console.log('click');
     if($(e.currentTarget).hasClass('active')){
@@ -11,3 +10,26 @@ $('h1').on('click',function(e){
         $(e.currentTarget).addClass('active');
     }
 });
+
+fetch('https://raphaelattal.com/api.php',{
+    method:'GET',
+    mode:'cors'
+})
+.then(resp => resp.json())
+.then(response => {
+    response.forEach(function(e,i){
+        $('body .scroll').append(`
+        <div class="quartier" style="animation-delay:${i/10}s">
+            <img src="${e.url}" />
+            <h2>${e.name}</h2>
+            <p>${e.city}</p>
+        </div>
+        `)
+    })
+})
+.catch(err => {
+    console.log('global error',err);
+})
+
+
+
